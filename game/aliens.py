1c83656dda93995ca5771ee3433efaaac2aa37c8
Exodia <67595890+DonRP@users.noreply.github.com>
2023-04-13 17:01:43 +0200

    test

--- game/aliens.py
+++ game/aliens.py
@@ -1,5 +1,6 @@
 import math
 import random
+from pythonpackages.renpygame.display import RenpyGameSurface
 
 # import basic pygame_sdl2 modules
 import renpy.display.transform as transform
@@ -180,147 +181,9 @@ class Score(pygame.sprite.Sprite):
 
 
 def main(winstyle=0):
-    # Initialize pygame
-    pygame.init()
 
-    # Set the display mode
-    if store._preferences.fullscreen:
-        winstyle = FULLSCREEN
-    else:
-        winstyle = 0
+    screen = RenpyGameSurface()
 
-    bestdepth = pygame.display.mode_ok(SCREENRECT.size, winstyle, 32)
-    screen = pygame.display.set_mode(SCREENRECT.size, winstyle, bestdepth)
-
-    # Load images, assign to sprite classes
-    # (do this before the classes are used, after screen setup)
-    img = load_image('player1.gif')
-    Player.images = [img, pygame.transform.flip(img, 1, 0)]
-    img = load_image('explosion1.gif')
-    Explosion.images = [img, pygame.transform.flip(img, 1, 1)]
-    Alien.images = load_images('alien1.gif', 'alien2.gif', 'alien3.gif')
-    Bomb.images = [load_image('bomb.gif')]
-    Shot.images = [load_image('shot.gif')]
-
-    # decorate the game window
-    icon = pygame.transform.scale(Alien.images[0], (32, 32))
-    pygame.display.set_icon(icon)
-    pygame.display.set_caption('Pygame Aliens')
-    pygame.mouse.set_visible(0)
-
-    # create the background, tile the bgd image
-    bgdtile = load_image('background.gif')
-    background = pygame.Surface(SCREENRECT.size)
-    for x in range(0, SCREENRECT.width, bgdtile.get_width()):
-        background.blit(bgdtile, (x, 0))
-    screen.blit(background, (0, 0))
-    pygame.display.flip()
-
-    # Initialize Game Groups
-    aliens = pygame.sprite.Group()
-    shots = pygame.sprite.Group()
-    bombs = pygame.sprite.Group()
-    all = pygame.sprite.RenderUpdates()
-    lastalien = pygame.sprite.GroupSingle()
-
-    # assign default groups to each sprite class
-    Player.containers = all
-    Alien.containers = aliens, all, lastalien
-    Shot.containers = shots, all
-    Bomb.containers = bombs, all
-    Explosion.containers = all
-    Score.containers = all
-
-    # Create Some Starting Values
-    global score
-    alienreload = ALIEN_RELOAD
-    kills = 0
-    clock = pygame.time.Clock()
-
-    global SCORE
-    SCORE = 0
-
-    # initialize our starting sprites
-    player = Player()
-    Alien()  # note, this 'lives' because it goes into a sprite group
-
-    # TODO: has been commented pe make it work
-    # if pygame.font:
-    #     all.add(Score())
-
-    while player.alive():
-
-        # TODO: has been commented pe make it work
-        # get input
-        # for event in pygame.event.get():
-        #     if event.type == QUIT or \
-        #         (event.type == KEYDOWN and event.key == K_ESCAPE):
-        #             return SCORE
-        # keystate = pygame.key.get_pressed()
-
-        # clear/erase the last drawn sprites
-        all.clear(screen, background)
-
-        # update all the sprites
-        all.update()
-
-        # TODO: has been commented pe make it work
-        # handle player input
-        # direction = keystate[K_RIGHT] - keystate[K_LEFT]
-        # player.move(direction)
-        # firing = keystate[K_SPACE]
-        # if not player.reloading and firing and len(shots) < MAX_SHOTS:
-        #     Shot(player.gunpos())
-        #     shoot_sound.play()
-        # player.reloading = firing
-
-        # Create new alien
-        if alienreload:
-            alienreload = alienreload - 1
-        elif not int(random.random() * ALIEN_ODDS):
-            Alien()
-            alienreload = ALIEN_RELOAD
-
-        # Drop bombs
-        if lastalien and not int(random.random() * BOMB_ODDS):
-            Bomb(lastalien.sprite)
-
-        # Detect collisions
-        for alien in pygame.sprite.spritecollide(player, aliens, 1):
-            # boom_sound.play()
-            Explosion(alien)
-            Explosion(player)
-            SCORE = SCORE + 1
-            player.kill()
-
-        for alien in pygame.sprite.groupcollide(shots, aliens, 1, 1).keys():
-            # boom_sound.play()
-            Explosion(alien)
-            SCORE = SCORE + 1
-
-        for bomb in pygame.sprite.spritecollide(player, bombs, 1):
-            # boom_sound.play()
-            Explosion(player)
-            Explosion(bomb)
-            player.kill()
-
-        # draw the scene
-        dirty = all.draw(screen)
-        pygame.display.update(dirty)
-
-        # cap the framerate
-        clock.tick(40)
-
-    pygame.time.wait(1000)
-
-    # * https://github.com/DRincs-Productions/Renpygame/issues/3
-    # * renpytom tell me to use:
-    renpy.display_reset()  # but not work
-
-    # ! It's not work:
-    # ! because when renpy try to edit the screen not find it
-    renpy.call("start")
-    # It's work
-    renpy.call("retry")
+    renpy.show_screen("renpygame_surface", surface=screen)
 
     return SCORE
--- pythonpackages/renpygame/display.py
+++ pythonpackages/renpygame/display.py
@@ -6,25 +6,11 @@ import pythonpackages.renpygame.pygame as pygame
 
 
 class RenpyGameSurface(renpy.Displayable):
-    """pygame: https://www.pygame.org/docs/ref/surface.html
-    pygame_sdl2: https://github.com/renpy/pygame_sdl2/blob/48e9c45667152a4ccf98d6d9251eeb3c8858b5f1/src/pygame_sdl2/surface.pyx#L53"""
-
     def __init__(self, **kwargs):
         # renpy.Displayable init
         super(RenpyGameSurface, self).__init__(**kwargs)
 
         self.renderMainSurface = renpy.Render(300, 300)
-
-    def blit(
-        self,
-        source,  # Surface
-        dest: tuple[int, int],
-        area: Optional[tuple[int, int]] = None,
-        special_flags: int = 0
-    ):
-        # renpy.show_screen(
-        #     "rect", left=dest[0], top=dest[1], width=self.size[0], height=self.size[1])
-        return self.renderMainSurface.blit(source, (0, 0))
 
     def render(self, width, height, st, at) -> renpy.Render:
         # Create the render we will return.
@@ -32,10 +18,19 @@ class RenpyGameSurface(renpy.Displayable):
 
         # Create a render from the child.
         child_render = renpy.render(renpy.displayable(
-            "background.gif"), width, height, st, at)
+            "alien3.gif"), width, height, st, at)
+
+        file = renpy.open_file("images/background.gif")
+        image = pygame.image.load(file)
+        bgdtile = image.convert()
+        # bgdtile = renpy.displayable("background.gif")
+
+        background = pygame.Surface((300, 300))
+        background.blit(bgdtile, (300, 300))
+        child_render.blit(background, (300, 300))
 
         # Get the size of the child.
-        self.width, self.height = child_render.get_size()
+        # self.width, self.height = child_render.get_size()
 
         # Blit (draw) the child's render to our render.
         render.blit(child_render, (0, 0))
@@ -48,7 +43,7 @@ def set_mode(size: tuple[int, int] = (0, 0), flags: int = 0, depth: int = 0, dis
     """If it is commented out it will replace the renpy screen creating an error when returning to renpy. https://www.pygame.org/docs/ref/display.html#pygame.display.set_mode"""
     # * It has the job of replacing the original so nothing happens
     surface = RenpyGameSurface()
-    renpy.show_screen("renpygame_surface", surface=surface)
+    # renpy.show_screen("renpygame_surface", surface=surface)
     return surface
 
 
